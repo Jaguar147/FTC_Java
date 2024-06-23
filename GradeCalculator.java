206f79db2aea259ed43dfbbba47848eed622ed73
Jaguar147 <masoudakhan14@gmail.com>
2024-06-23 11:58:50 -0500

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-class HelloWorld {
+class GradeCalculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the GradeWhiz! Calculate your average instantly.");
         //Prompt for user to enter their grades for three subjects

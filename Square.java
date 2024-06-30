6ed3d9e0dba831d6cdc842ba7b8ba97152c67306
Jaguar147 <masoudakhan14@gmail.com>
2024-06-30 10:20:34 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,49 +1,62 @@
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @Autonomous
 
-public class MasoudaSquare extends LinearOpMode {
+public class MasoudaNewS extends LinearOpMode {
     DcMotor frontLeft;
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
-
-
 
     public void runOpMode() {
       frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
       frontRight = hardwareMap.get(DcMotor.class, "frontRight");
       backLeft = hardwareMap.get(DcMotor.class, "backLeft");
       backRight = hardwareMap.get(DcMotor.class, "backRight");
-    waitForStart();
-      int count = 0;
-      while (count < 4){
-         frontLeft.setPower(-0.7);
-         frontRight.setPower(0.7);
-         backLeft.setPower(-0.7);
-         backRight.setPower(0.7);
-          sleep(200);
-          
+
+         int count = 1;
+      while (count < 2){
+         frontLeft.setPower(-0.5);
+         frontRight.setPower(0.5);
+         backLeft.setPower(-0.5);
+         backRight.setPower(0.5);
+          sleep(1500);
          frontLeft.setPower(0);
          frontRight.setPower(0);
          backLeft.setPower(0);
          backRight.setPower(0);
-         sleep(100);
-         
-         frontLeft.setPower(-0.7);
-         frontRight.setPower(-0.7);
-         backLeft.setPower(-0.7);
-         backRight.setPower(-0.7);
-          sleep(200);
-         
-         count = (count++);
+          sleep(500);
+         frontLeft.setPower(-0.6);
+         frontRight.setPower(-0.6);
+         backLeft.setPower(0.6);
+         backRight.setPower(0.6);
+          sleep(1600);
+         frontLeft.setPower(0);
+         frontRight.setPower(0);
+         backLeft.setPower(0);
+         backRight.setPower(0);
+          sleep(500);
+         frontLeft.setPower (0.5);
+         frontRight.setPower(-0.5);
+         backLeft.setPower (0.5);
+         backRight.setPower(-0.5);
+          sleep(1500);
+         frontLeft.setPower(0);
+         frontRight.setPower(0);
+         backLeft.setPower(0);
+         backRight.setPower(0);
+          sleep(500);
+         frontLeft.setPower(0.6);
+         frontRight.setPower(0.6);
+         backLeft.setPower(-0.6);
+         backRight.setPower(-0.6);
+          sleep(1600);
+           count++;
       }
-
     }
-    
 }

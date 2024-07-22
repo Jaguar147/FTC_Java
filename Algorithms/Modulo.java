75c426b5f607fb383f754f719cc9090a6fc0d01d
Jaguar147 <masoudakhan14@gmail.com>
2024-07-21 23:00:54 -0500

    Create Modulo.java

---
+++ Algorithms/Modulo.java
@@ -0,0 +1,17 @@
+//import java.util.Scanner; 
+
+//Even vs Odd - Modulo Operator
+    
+    System.out.println("_________________"); 
+    System.out.println("Modulo Operation"); 
+    
+    for (int v = 1 ; v <= 10; v++) {
+        int n = v % 2 ;
+    System.out.println(v);
+
+    if (n == 1) {
+        System.out.println("The odd number is: " + v);
+    }
+    else {
+         System.out.println("The even number is: " + v);
+    }

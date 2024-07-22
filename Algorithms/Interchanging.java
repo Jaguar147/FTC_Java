ee69c8bd70371666f171e5d2d448092fd2f0806a
Jaguar147 <masoudakhan14@gmail.com>
2024-07-21 23:00:04 -0500

    Create Interchanging.java

---
+++ Algorithms/Interchanging.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner; 
+
+System.out.println("_________");
+    System.out.println("Exchange");
+    
+  int a = 5;
+  int b = 7; 
+     System.out.println("Before swapping : a = " + a + " and b = " + b);
+
+  
+    int h = a;
+    a = b;
+    b = h;

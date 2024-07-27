1722a9373e8755a1ec18ee14939a56ed406f4907
Jaguar147 <masoudakhan14@gmail.com>
2024-07-27 12:36:12 -0500

    Create GreatestOfThree.java

---
+++ Algorithms/GreatestOfThree.java
@@ -0,0 +1,13 @@
+    int a = 5;
+    int b = 3;
+    int c = 2;
+    
+    if (a > b && a > c) {
+         System.out.println("a is greatest");
+    }
+    else if (b > c && b > a) {
+         System.out.println("b is greatest");
+    }
+    else if (c > b && c > a) {
+         System.out.println("c is greatest");
+    }

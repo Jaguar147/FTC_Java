ee22ab314dae7b1e0907c1df1d738e18f027b291
Jaguar147 <masoudakhan14@gmail.com>
2024-07-27 12:56:19 -0500

    Create LetterOrNot.java

---
+++ Algorithms/LetterOrNot.java
@@ -0,0 +1,8 @@
+ char character = 'a';
+     if ((character >= 'a' && character <= 'z') ||
+            (character >= 'A' && character <= 'Z')) {
+        System.out.println("Letter");
+            }
+    else {
+        System.out.println("Not letter");
+    }

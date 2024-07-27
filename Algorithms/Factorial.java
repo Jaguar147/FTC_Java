2e620fb84b39999715bb32f46b1b5e94d8844d5b
Jaguar147 <masoudakhan14@gmail.com>
2024-07-27 13:14:52 -0500

    Update Inp_Factorial.java

--- Algorithms/Factorial.java
+++ Algorithms/Factorial.java
@@ -1,6 +1,20 @@
- int sum = 1;
+/*
+IF WANT NO INPUT
+int sum = 1;
         
         for (int i = 1 ; i <= 10; i++) {
             sum = sum * i ;
         System.out.println(sum);
+        } */
+
+// WITH THE INPUT
+ System.out.print("Enter a number: ");
+    Scanner num = new Scanner(System.in);
+    int n = num.nextInt();
+
+    int sum = 1;
+        
+        for (int i = 1 ; i <= n; i++) {
+            sum = sum * i ;
         }
+         System.out.println("Factorial of " + n + " is : " + sum);

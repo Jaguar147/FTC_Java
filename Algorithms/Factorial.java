c6860642538b93396eb9cd6d87e53fb871dc22a9
Jaguar147 <masoudakhan14@gmail.com>
2024-07-27 13:21:36 -0500

    Update Factorial.java

--- Algorithms/Factorial.java
+++ Algorithms/Factorial.java
@@ -1,20 +1,13 @@
-/*
-IF WANT NO INPUT
-int sum = 1;
-        
-        for (int i = 1 ; i <= 10; i++) {
-            sum = sum * i ;
-        System.out.println(sum);
-        } */
-
-// WITH THE INPUT
- System.out.print("Enter a number: ");
+//Finding Factorial
+    System.out.print("Enter a number: ");
     Scanner num = new Scanner(System.in);
-    int n = num.nextInt();
+    float n = num.nextFloat();
 
-    int sum = 1;
-        
+    double sum = 1;
+
         for (int i = 1 ; i <= n; i++) {
             sum = sum * i ;
         }
          System.out.println("Factorial of " + n + " is : " + sum);
+    
+  }
